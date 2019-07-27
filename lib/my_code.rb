025cbc3982fb5922ee2d861bbf76c778295a97f6
George Kirby <georgeskirby@btinternet.com>
2019-07-27 17:36:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,11 +10,12 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_point = true)
+def reduce(source_array, starting_point = 0)
   keep_track = starting_point
   
   for i in source_array
     keep_track = yield(keep_track, i)
+    puts keep_track
   end
   
   keep_track
